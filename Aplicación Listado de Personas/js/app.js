793765781c3eaef84e269fd68f6632765c579bbf
matiasrodriguezc01 <matiasrodriguezc01@gmail.com>
2024-10-28 01:04:29 -0300

    files uploading

---
+++ Aplicación Listado de Personas/js/app.js
@@ -0,0 +1,27 @@
+const personas = [
+    new Persona('John', 'Doe'),
+    new Persona('Matías', 'Rodríguez')
+]
+
+function mostrarPersonas(){
+    let texto = '';
+    for (let persona of personas){
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if (nombre.value != '' && apellido.value != '' ){
+        const persona = new Persona(nombre.value, apellido.value);
+        personas.push(persona);
+        mostrarPersonas();
+    }
+    else {
+        console.log('No hay información para agregar');
+    }
+    
+}
